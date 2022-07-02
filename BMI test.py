6f20e43ba362ed978ff5c52a40219a98aa0c8303
Doris.Xu <dorisxjt@gmail.com>
2022-07-02 23:32:45 +1000

    BMI module finished

---
+++ BMI test.py
@@ -0,0 +1,16 @@
+weight = float(input("Weight(KG): \n"))
+#weight should be kg
+height = float(input("Height(CM): \n"))
+#height should be cm
+BMI = weight / (height/100)**2
+
+if BMI <= 18.5:
+    print("Underweight")
+elif BMI <= 25.0:
+    print("Normal")
+elif BMI <= 30.0:
+    print("Overweight")
+else:
+    print("Obese")
+
+    
